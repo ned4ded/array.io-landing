a820a0301b1a1c4205e6d81dff755592d2dfefa4
Bazhenov Eugen <wtf.czabe@gmail.com>
2018-09-27 19:39:07 +0300

    add cube animation

--- src/scripts/terminal.js
+++ src/scripts/terminal.js
@@ -164,8 +164,6 @@ document.addEventListener('DOMContentLoaded', (() => {
     }
 
     const liningListener = (ev) => {
-      console.log('lining');
-
       ev.preventDefault();
 
       lining.removeEventListener('click', liningListener);
@@ -180,5 +178,152 @@ document.addEventListener('DOMContentLoaded', (() => {
     toggler.addEventListener('click', listener);
   });
 
+  class CubeImage {
+    constructor(el, openTime = 500) {
+      this.el = el;
+      this.openTime = openTime;
+
+      this.vacate();
+
+      this.setState('closed');
+    }
+
+    setState(state) {
+      switch (state) {
+        case 'open':
+          this.state = 'open';
+          break;
+        case 'closed':
+          this.state = 'closed';
+          break;
+        case 'opening':
+          this.state = 'opening';
+          break;
+        case 'closing':
+          this.state = 'closing';
+          break;
+        default:
+          return new Error('ModalWindow: Wrong State');
+      }
+
+      this.notify();
+
+      return this;
+    }
+
+    notify() {
+      this.el.dataset.animationProcess = this.state;
+    }
+
+    getState() {
+      return this.state;
+    }
+
+    isState(state) {
+      return this.getState() === state;
+    }
+
+    open(cb = () => {}) {
+      if(this.isState('open') || this.isBusy()) return false;
+
+      this.occupy();
+      this.setState('opening');
+
+      setTimeout(() => {
+        this.setState('open');
+        this.vacate();
+
+        return cb();
+      }, this.openTime);
+
+      return this;
+    }
+
+    close(cb = () => {}) {
+      if(this.isState('closed') || this.isBusy()) return false;
+
+      this.occupy();
+      this.setState('closing');
+
+      setTimeout(() => {
+        this.setState('closed');
+        this.vacate();
+
+        return cb();
+      }, this.openTime);
+
+      return this;
+    }
+
+    isBusy() {
+      return !!this.busy;
+    }
+
+    occupy() {
+      this.busy = true;
+    }
+
+    vacate() {
+      this.busy = false;
+    }
+  }
+
+  const cubeEl = document.querySelector('[data-animation-process]');
+  const cube = new CubeImage(cubeEl);
+
+  const getCubePosition = (top, height) => {
+    const bottom = top + height;
+    const outTop = (top + height * 0.1) < 0;
+    const outBottom = (top + height * 0.9) - window.innerHeight > 0;
+
+    if(outTop || outBottom) {
+      return 'out';
+    } else {
+      return 'in';
+    }
+  }
+
+  const changeCubeState = (ev) => {
+    const { top, height } = cubeEl.getBoundingClientRect();
+
+    window.removeEventListener('scroll', changeCubeState);
+
+
+    if(cube.isState('closed') && getCubePosition(top, height) === 'in') {
+      cube.open(() => {
+        changeCubeState();
+      });
+
+      return;
+    } else if(cube.isState('open') && getCubePosition(top, height) === 'out') {
+      cube.close(() => {
+        changeCubeState();
+      });
+
+      return;
+    }
+
+    window.addEventListener('scroll', changeCubeState);
+
+    return;
+  }
+
+  changeCubeState();
+
+  const path = anime.path('#cube-path-top');
+
+  anime({
+    targets: '#cube-path-top',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 1500,
+    delay: function(el, i) { return i * 250 },
+  });
+
+  anime({
+    targets: '#cube-point-top',
+    cx: path('x'), // Follow the x values from the path `Object`
+    cy: path('y'), // Follow the y values from the path `Object`
+  })
 
 }), false);
