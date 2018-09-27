3a731d0928b873fdc6eb395bbf52adb51ebdf117
Bazhenov Eugen <wtf.czabe@gmail.com>
2018-09-27 09:09:29 +0300

    fix spelling, add ModalWindow class, set up popup behavior, adjust text on index page, fill up the instruction, style the homescreen

--- src/scripts/terminal.js
+++ src/scripts/terminal.js
@@ -49,5 +49,136 @@ document.addEventListener('DOMContentLoaded', (() => {
     return instruction ? new Swiper(instruction, options) : null;
   })();
 
+  class ModalWindow {
+    constructor(element, lining, timeline = 500) {
+      this.el = element;
+      this.lining = lining;
+      this.timeline = timeline;
+      this.busy = false;
+
+      this.setState('closed');
+    }
+
+    setState(state) {
+      switch (state) {
+        case 'open':
+          this.state = 'open';
+          break;
+        case 'closed':
+          this.state = 'closed';
+          break;
+        case 'opening':
+          this.state = 'opening';
+          break;
+        case 'closing':
+          this.state = 'closing';
+          break;
+        default:
+          return new Error('ModalWindow: Wrong State');
+      }
+
+      this.notify();
+
+      return this;
+    }
+
+    notify() {
+      this.el.dataset.modalState = this.state;
+      this.lining.dataset.modalState = this.state;
+    }
+
+    getState() {
+      return this.state;
+    }
+
+    isState(state) {
+      return this.getState() === state;
+    }
+
+    open(cb = () => {}) {
+      if(this.isState('open') || this.isBusy()) return false;
+
+      this.occupy();
+      this.setState('opening');
+
+      setTimeout(() => {
+        this.setState('open');
+        this.vacate();
+
+        return cb();
+      }, this.timeline);
+
+      return this;
+    }
+
+    close(cb = () => {}) {
+      if(this.isState('closed') || this.isBusy()) return false;
+
+      this.occupy();
+      this.setState('closing');
+
+      setTimeout(() => {
+        this.setState('closed');
+        this.vacate();
+
+        return cb();
+      }, this.timeline);
+
+      return this;
+    }
+
+    isBusy() {
+      return !!this.busy;
+    }
+
+    occupy() {
+      this.busy = true;
+    }
+
+    vacate() {
+      this.busy = false;
+    }
+  }
+
+  const lining = document.getElementById('lining');
+
+  const modals = Array.from( document.querySelectorAll('[data-modal-toggler]') ).map(el => {
+    const id = el.dataset.modalToggler;
+    const modal = document.getElementById(id);
+
+    return modal ? { toggler: el, modal: new ModalWindow(modal, lining) } : null;
+  });
+
+  modals.forEach(({ toggler, modal }) => {
+    const listener = (ev) => {
+      ev.preventDefault();
+
+      toggler.removeEventListener('click', listener);
+
+      modal.open(() => {
+        lining.addEventListener('click', liningListener);
+
+        return;
+      });
+
+    }
+
+    const liningListener = (ev) => {
+      console.log('lining');
+
+      ev.preventDefault();
+
+      lining.removeEventListener('click', liningListener);
+
+      modal.close(() => {
+        toggler.addEventListener('click', listener);
+
+        return;
+      })
+    }
+
+    toggler.addEventListener('click', listener);
+  });
+
 
 }), false);
